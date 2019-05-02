4bca3047b1157024a9adaeaa28608311edfc697d
Darren <darren.ccy17@gmail.com>
2019-05-02 19:26:04 +0800

    boom sound
    ver.Darren

--- Battleship/src/GameResources.cs
+++ Battleship/src/GameResources.cs
@@ -61,7 +61,7 @@ public static class GameResources
 	private static void LoadSounds()
 	{
         NewSound("Error", "error.wav");
-		NewSound("Hit", "hit.wav");
+		NewSound("Hit", "boom.wav");
 		NewSound("Sink", "sink.wav");
 		NewSound("Siren", "siren.wav");
 		NewSound("Miss", "watershot.wav");
